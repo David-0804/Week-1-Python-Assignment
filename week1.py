77a68772886ea25ab4ae3aee17483aa6c4d8022e
David <ochiengsdavids@gmail.com>
2025-07-29 19:45:54 +0300

    Week 1 Python Assignment

--- week1.py
+++ week1.py
@@ -2,7 +2,6 @@ num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 operation = input("Enter an operation (+, -, *, /): ")
 
-# Perform the selected operation
 if operation == '+':
     result = num1 + num2
     print(f"{num1} + {num2} = {result}")
